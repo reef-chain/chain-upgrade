5c887f5655507d224e6431efaf23c4d50ec4b815
arunjot12 <arunjot@antiersolutions.com>
2025-08-20 11:04:05 +0530

    fix: Removed warnings

--- modules/evm/src/lib.rs
+++ modules/evm/src/lib.rs
@@ -855,6 +855,8 @@ impl<T: Config> Pallet<T> {
         }
 
         Accounts::<T>::remove(address);
+
+        #[allow(deprecated)]
         AccountStorages::<T>::remove_prefix(address, None);
 
         Ok(size)
@@ -1091,6 +1093,7 @@ impl<T: Config> Pallet<T> {
                 .take()
                 .ok_or(Error::<T>::ContractNotFound)?;
 
+            #[allow(deprecated)]
             AccountStorages::<T>::remove_prefix(contract, None);
 
             CodeInfos::<T>::mutate_exists(&contract_info.code_hash, |maybe_code_info| {
